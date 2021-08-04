e0f87ff7eb5340fbdd504dc0e06aad3f61619143
srzh <19srzh88@gmail.com>
2021-08-04 21:51:45 +0400

    adding auth screen textformfields

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -10,8 +10,83 @@ class AuthWidget extends StatefulWidget {
 class _AuthWidgetState extends State<AuthWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Войти',
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            FormWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
+
+class FormWidget extends StatefulWidget {
+  const FormWidget({Key? key}) : super(key: key);
+
+  @override
+  _FormWidgetState createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<FormWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        buildEmailFormField(),
+        const SizedBox(height: 10),
+        buildPasswordFormField(),
+      ],
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      autofocus: true,
+      decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          filled: true,
+          labelText: 'Почта',
+          fillColor: Colors.white),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple),
+          ),
+          filled: true,
+          labelText: 'Пароль',
+          fillColor: Colors.white),
     );
   }
 }
