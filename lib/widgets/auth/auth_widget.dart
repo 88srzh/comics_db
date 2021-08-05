33a8b5821ae3ec173747a9f66dd453e27a0561e5
srzh <19srzh88@gmail.com>
2021-08-05 16:30:49 +0400

    adding auth scren

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -9,6 +9,16 @@ class AuthWidget extends StatefulWidget {
 }
 
 class _AuthWidgetState extends State<AuthWidget> {
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+
+    void _resetPassword() {}
+
+    void _auth() {
+      final login = _loginTextController.text;
+      final password = _passwordTextController;
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,34 +37,92 @@ class _AuthWidgetState extends State<AuthWidget> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FormWidget(),
+            // const FormWidget(),
+            Column(
+      children: [
+        buildEmailFormField(),
+        const SizedBox(height: 10),
+        buildPasswordFormField(),
+      ],
+    ),
             const SizedBox(height: 5.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
-                Text(
-                  'Забыли пароль?',
-                  style: TextStyle(color: kPrimaryColor),
-                ),
-              ],
-            ),
+            resetPassword(),
             const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Войти', style: TextStyle(fontSize: 24)),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                foregroundColor: MaterialStateProperty.all(kPrimaryColor),
-                padding: MaterialStateProperty.all(
-                  const EdgeInsets.symmetric(horizontal: 130.0, vertical: 15.0),
-                ),
-                // TODO: add border to button
-              ),
-            ),
+            loginButton(),
+            const SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text('Нет аккаунта? '),
+                Text('Зарегистрируйтесь'),
+              ],
+            )
           ],
         ),
       ),
     );
+  }
+
+  TextFormField buildEmailFormField() {
+    return  TextFormField(
+      controller: _loginTextController,
+      autofocus: true,
+      decoration: formFieldInputDecoration('Почта'),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      controller: _passwordTextController,
+      obscureText: true,
+      decoration: formFieldInputDecoration('Пароль'),
+    );
+  }
+
+    InputDecoration formFieldInputDecoration(String labelText) {
+    return InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+        labelStyle: const TextStyle(
+          color: Colors.black54,
+        ),
+        border: const OutlineInputBorder(),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: kPrimaryColor),
+        ),
+        filled: true,
+        labelText: labelText,
+        fillColor: Colors.white);
+  }
+
+  ElevatedButton loginButton() {
+    return ElevatedButton(
+    onPressed: () => _auth(),
+    child: const Text('Войти', style: TextStyle(fontSize: 24)),
+    style: ButtonStyle(
+      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))),
+      side: MaterialStateProperty.all(const BorderSide(color: kPrimaryColor)),
+      backgroundColor: MaterialStateProperty.all(Colors.white),
+      foregroundColor: MaterialStateProperty.all(kPrimaryColor),
+      padding: MaterialStateProperty.all(
+        const EdgeInsets.symmetric(horizontal: 130.0, vertical: 15.0),
+      ),
+    ),
+  );
+}
+
+  Row resetPassword() {
+    return Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              InkWell(
+                onTap: () => _resetPassword(),
+                child: const Text(
+                  'Забыли пароль?',
+                  style: TextStyle(color: kPrimaryColor),
+                ),
+              ),
+            ],
+          );
   }
 }
 
@@ -72,50 +140,3 @@ class HeaderWidget extends StatelessWidget {
   }
 }
 
-class FormWidget extends StatefulWidget {
-  const FormWidget({Key? key}) : super(key: key);
-
-  @override
-  _FormWidgetState createState() => _FormWidgetState();
-}
-
-class _FormWidgetState extends State<FormWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        buildEmailFormField(),
-        const SizedBox(height: 10),
-        buildPasswordFormField(),
-      ],
-    );
-  }
-
-  TextFormField buildEmailFormField() {
-    return customTextFormField('Почта', false, true);
-  }
-
-  TextFormField buildPasswordFormField() {
-    return customTextFormField('Пароль', true, false);
-  }
-
-  TextFormField customTextFormField(
-      String labelText, bool obskureText, bool autofocus) {
-    return TextFormField(
-      autofocus: autofocus,
-      obscureText: obskureText,
-      decoration: InputDecoration(
-          contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
-          labelStyle: const TextStyle(
-            color: Colors.black54,
-          ),
-          border: const OutlineInputBorder(),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: kPrimaryColor),
-          ),
-          filled: true,
-          labelText: labelText,
-          fillColor: Colors.white),
-    );
-  }
-}
