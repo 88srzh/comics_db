db719bd5646a8a6ced99a86afb4db993eb57e076
srzh <19srzh88@gmail.com>
2021-08-12 22:46:20 +0400

    added api client

--- lib/domain/api_client/api_client.dart
+++ lib/domain/api_client/api_client.dart
@@ -2,7 +2,32 @@ import 'dart:convert';
 import 'dart:io';
 
 class ApiClient {
-  final client = HttpClient();}
+  final _client = HttpClient();
+  static const _host = 'https://api.themoviedb.org/3';
+  static const _imageUrl = 'http://image.tmbd.org/t/p/w500';
+  static const _apiKey = '16257858b79d98176712e6d572638798';
+
+  Future<String> makeToken() async {
+    final url = Uri.parse(
+        'https://api.themoviedb.org/3/authentication/token/new?api_key=$_apiKey');
+    final request = await _client.getUrl(url);
+    final response = await request.close();
+    final json = await response
+        .transform(utf8.decoder)
+        .toList()
+        .then((value) => value.join())
+        .then((v) => jsonDecode(v) as Map<String, dynamic>);
+    final token = json['request_token'] as String;
+    return token;
+  }
+
+  // shikimori
+  // static const _apiKey = '7AINHIOa4QS4hJlVdX8l0EaSBnbxAB3h1JT4FJ9UI7k';
+  // access token
+  // 7AINHIOa4QS4hJlVdX8l0EaSBnbxAB3h1JT4FJ9UI7k
+  // refresh token
+  // K5MkVnDh6HAO5BauUZH1AqVaIGfdmJRNYnAxljS2Y94
+}
 
   // Future<List<Post>> getPost() async {
   //   final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
