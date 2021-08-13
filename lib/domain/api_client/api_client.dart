d1cb1ab21d4d361d0ecfae6f7525118aff97e0c8
srzh <19srzh88@gmail.com>
2021-08-13 15:43:29 +0400

    completed api client

--- lib/domain/api_client/api_client.dart
+++ lib/domain/api_client/api_client.dart
@@ -7,19 +7,73 @@ class ApiClient {
   static const _imageUrl = 'http://image.tmbd.org/t/p/w500';
   static const _apiKey = '16257858b79d98176712e6d572638798';
 
+  Uri _makeUri(String path, [Map<String, dynamic>? parameters]) {
+        final uri = Uri.parse('$_host$path?api_key=$_apiKey');
+        if (parameters != null) {
+          return uri.replace(queryParameters: parameters);
+        } else {
+          return uri;
+        }
+  }
+
   Future<String> makeToken() async {
-    final url = Uri.parse(
-        'https://api.themoviedb.org/3/authentication/token/new?api_key=$_apiKey');
+    final url = _makeUri('/authentication/token/new', <String, dynamic>{'api_key': _apiKey});
     final request = await _client.getUrl(url);
     final response = await request.close();
-    final json = await response
-        .transform(utf8.decoder)
-        .toList()
-        .then((value) => value.join())
-        .then((v) => jsonDecode(v) as Map<String, dynamic>);
+   final json = (await response.jsonDecode()) as Map<String, dynamic>;
     final token = json['request_token'] as String;
     return token;
   }
+
+  Future<String> validateUser({
+    required String username,
+    required String password,
+    required String requestToken
+    }) async {
+    final url = _makeUri('/authentication/token/validate_with_login', <String, dynamic>{'api_key': _apiKey});
+    final parameters = <String, dynamic> {
+      'username' : username,
+      'password' : password,
+      'request_token' : requestToken,
+    };
+    final  request = await _client.postUrl(url);
+    request.headers.contentType = ContentType.json;
+    request.write(jsonEncode(parameters));
+    final response = await request.close();
+
+    final json = (await response.jsonDecode()) as Map<String, dynamic>;
+
+    final token = json['request_token'] as String;
+    return token;
+  }
+
+  Future<String> makeSession({
+    required String requestToken
+    }) async {
+      final url = _makeUri('/authentication/session/new', <String, dynamic>{'api_key': _apiKey});
+    final parameters = <String, dynamic> {
+      'request_token' : requestToken,
+    };
+    final  request = await _client.postUrl(url);
+    request.headers.contentType = ContentType.json;
+    request.write(jsonEncode(parameters));
+    final response = await request.close();
+
+    final json = (await response.jsonDecode()) as Map<String, dynamic>;
+    final sessionId = json['session_id'] as String;
+    return sessionId;
+  }
+}
+
+extension HttpClientResponseJsonDecode on HttpClientResponse {
+  Future<dynamic> jsonDecode() async {
+    transform(utf8.decoder)
+        .toList()
+        .then((value) => value.join())
+        .then<dynamic>((v) => json.decode(v));
+
+  }
+}
 
   // shikimori
   // static const _apiKey = '7AINHIOa4QS4hJlVdX8l0EaSBnbxAB3h1JT4FJ9UI7k';
@@ -27,39 +81,3 @@ class ApiClient {
   // 7AINHIOa4QS4hJlVdX8l0EaSBnbxAB3h1JT4FJ9UI7k
   // refresh token
   // K5MkVnDh6HAO5BauUZH1AqVaIGfdmJRNYnAxljS2Y94
-}
-
-  // Future<List<Post>> getPost() async {
-  //   final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-  //   final request = await client.getUrl(url);
-  //   final response = await request.close();
-  //   final json = await response
-  //   .transform(utf8.decoder)
-  //   .toList()
-  //   .then((value) => value.join())
-  //   .then((v) => jsonDecode(v) as List<dynamic>);
-  //   final result = json
-  //   .map((dynamic e) => Post.fromJson (e as Map<String, dynamic>))
-  //   .toList();
-  //   return result;
-  // }
-
-  // Future<Post> createPost({required String title, required String body}) async {
-  //   final url = uri.parse('https://jsonplaceholder.typicode.com/posts');
-  //   final parameters = <String, dynamic> {
-  //     'title' : title,
-  //     'body' : body,
-  //     'userId' : 109
-  //   };
-  //   final request = await client.postUrl(url);
-  //   request.header.add('Content-type', 'application/json; charset=UTF-8');
-  //   request.write(jsonEncode(parameters));
-  //   final response = await request.close();
-  //   final string = await response
-  //   .transform(utf8.decoder)
-  //   .toList()
-  //   .then((value) => value.join());
-  //   final json = jsonDecode(string) as Map<String, dynamic>;
-  //   final post = Post.fromJson(json);
-  //   return post;
-  // }
