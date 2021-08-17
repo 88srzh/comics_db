484e9c38944397d7af67f45429df53a60d88f4bb
srzh <61797666+88srzh@users.noreply.github.com>
2021-08-17 14:35:45 +0400

    started doing error handling

--- lib/domain/api_client/api_client.dart
+++ lib/domain/api_client/api_client.dart
@@ -1,6 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
 
+/*
+1. нет сети
+2. нет ответа, таймаут соединения
+3. сервер недоступен
+4. сервер не может обработать запрашиваемый запрос
+5. сервер ответил не то, что мы ожидали
+6. сервер ответил ожидаемой ошибкой
+*/
+
 class ApiClient {
   final _client = HttpClient();
   static const _host = 'https://api.themoviedb.org/3';
