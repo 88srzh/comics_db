0edf46d1440ee8d57f9ca4a3c8cd5540212d0545
srzhik <19srzh88@gmail.com>
2022-01-22 10:31:37 +0400

    small fixes

--- lib/ui/widgets/auth/auth_model.dart
+++ lib/ui/widgets/auth/auth_model.dart
@@ -68,20 +68,3 @@ class AuthModel extends ChangeNotifier {
     );
   }
 }
-
-// class AuthProvider extends InheritedNotifier {
-//   final AuthModel model;
-
-//   const AuthProvider({Key? key, required this.model, required Widget child})
-//       : super(key: key, notifier: model, child: child);
-
-//   static AuthProvider? watch(BuildContext context) {
-//     return context.dependOnInheritedWidgetOfExactType<AuthProvider>();
-//   }
-
-//   static AuthProvider? read(BuildContext context) {
-//     final widget =
-//         context.getElementForInheritedWidgetOfExactType<AuthProvider>()?.widget;
-//     return widget is AuthProvider ? widget : null;
-//   }
-// }
