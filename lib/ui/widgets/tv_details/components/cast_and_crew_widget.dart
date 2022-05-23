94e7b1dc5dc2a37636ab0547d20e2ac81e30fe5e
srzh <19srzh88@gmail.com>
2022-05-23 20:25:47 +0400

    refactoring and added cast widget

---
+++ lib/ui/widgets/tv_details/components/cast_and_crew_widget.dart
@@ -0,0 +1,114 @@
+import 'package:comics_db_app/app_colors.dart';
+import 'package:comics_db_app/domain/api_client/image_downloader.dart';
+import 'package:comics_db_app/resources/resources.dart';
+import 'package:comics_db_app/ui/widgets/tv_details/tv_details_model.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class TvCastWidget extends StatelessWidget {
+  const TvCastWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Full Cast & Crew',
+                style: TextStyle(
+                  fontSize: 21,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.genresText,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 250.0,
+              child: Scrollbar(child: _TvActorListWidget()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TvActorListWidget extends StatelessWidget {
+  const _TvActorListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<TvDetailsModel>(context, listen: true);
+    var cast = model.tvDetails?.credits.cast;
+    if (cast == null || cast.isEmpty) return const SizedBox.shrink();
+    return ListView.builder(
+      itemCount: cast.length,
+      itemExtent: 120,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context, int index) {
+        return _TvActorListItemWidget(actorIndex: index);
+      },
+    );
+  }
+}
+
+class _TvActorListItemWidget extends StatelessWidget {
+  final int actorIndex;
+
+  const _TvActorListItemWidget({Key? key, required this.actorIndex}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<TvDetailsModel>(context, listen: false);
+    final actor = model.tvDetails?.credits.cast[actorIndex];
+    final backdropPath = actor?.profilePath;
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black.withOpacity(0.2)),
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.purple.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          clipBehavior: Clip.hardEdge,
+          child: Column(
+            children: [
+              backdropPath != null
+                  ? Image.network(ImageDownloader.imageUrl(backdropPath))
+                  // : const SizedBox.shrink(),
+                  : const Image(image: AssetImage(AppImages.noImage)),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(actor!.name, maxLines: 1),
+                      const SizedBox(height: 7),
+                      Text(actor.character, maxLines: 2),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
