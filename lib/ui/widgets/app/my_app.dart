30935849ec430ab61f96db1bb2ce2495dfeef9a9
srzhik <19srzh88@gmail.com>
2022-05-31 15:13:06 +0400

    add custom behavior to fix listview scrolling effect

--- lib/ui/widgets/app/my_app.dart
+++ lib/ui/widgets/app/my_app.dart
@@ -1,17 +1,24 @@
 import 'package:comics_db_app/app_colors.dart';
+import 'package:comics_db_app/ui/components/custom_behavior.dart';
 import 'package:comics_db_app/ui/navigation/main_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-
 class MyApp extends StatelessWidget {
   // TODO: нужно отказаться от mainNavigation
   static final mainNavigation = MainNavigation();
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: CustomBehavior(),
+          child: child!,
+        );
+      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
@@ -21,18 +28,17 @@ class MyApp extends StatelessWidget {
           ),
         ),
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            selectedItemColor: AppColors.bottomBarBackgroundColor,
-            unselectedItemColor: AppColors.bottomBarIconColor),
+            selectedItemColor: AppColors.bottomBarBackgroundColor, unselectedItemColor: AppColors.bottomBarIconColor),
       ),
-    localizationsDelegates: const [
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-  ],
-  supportedLocales: const [
-    Locale('ru','RU'),
-    Locale('en',''),
-  ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ru', 'RU'),
+        Locale('en', ''),
+      ],
       routes: mainNavigation.routes,
       initialRoute: MainNavigationRouteNames.loaderWidget,
       onGenerateRoute: mainNavigation.onGenerateRoute,
