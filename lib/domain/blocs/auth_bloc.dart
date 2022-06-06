792aaa8732269a94676b121d402e93afd4fc7419
srzh <19srzh88@gmail.com>
2022-06-06 20:55:30 +0400

    adding bloc to loader model

--- lib/domain/blocs/auth_bloc.dart
+++ lib/domain/blocs/auth_bloc.dart
@@ -28,18 +28,55 @@ enum AuthStateStatus {
 
 abstract class AuthState {}
 
-class AuthAuthorizedState extends AuthState {}
+class AuthAuthorizedState extends AuthState {
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is AuthAuthorizedState && runtimeType == other.runtimeType;
 
-class AuthUnauthorizedState extends AuthState {}
+  @override
+  int get hashCode => 0;
+}
+
+class AuthUnauthorizedState extends AuthState {
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is AuthUnauthorizedState && runtimeType == other.runtimeType;
+
+  @override
+  int get hashCode => 0;
+}
 
 class AuthFailureState extends AuthState {
   // TODO: input error
   final Object error;
 
   AuthFailureState(this.error);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is AuthFailureState && runtimeType == other.runtimeType && error == other.error;
+
+  @override
+  int get hashCode => error.hashCode;
 }
 
-class AuthInProgressState extends AuthState {}
+class AuthInProgressState extends AuthState {
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is AuthInProgressState && runtimeType == other.runtimeType;
+
+  @override
+  int get hashCode => 0;
+}
+
+class AuthCheckStatusInProgressState extends AuthState {
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is AuthCheckStatusInProgressState && runtimeType == other.runtimeType;
+
+  @override
+  int get hashCode => 0;
+}
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final _sessionDataProvider = SessionDataProvider();
--- lib/ui/widgets/loader_widget/loader_view_model.dart
+++ lib/ui/widgets/loader_widget/loader_view_model.dart
@@ -1,6 +1,13 @@
+import 'package:bloc/bloc.dart';
 import 'package:comics_db_app/domain/services/auth_service.dart';
 import 'package:comics_db_app/ui/navigation/main_navigation.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+enum LoaderViewCubitState { unknown, authorized, notAuthorized }
+
+class LoaderViewCubit extends Cubit<LoaderViewCubit> {
+  LoaderViewCubit(LoaderViewCubit initialState) : super(initialState);
+}
 
 class LoaderViewModel {
   final BuildContext context;
@@ -16,9 +23,7 @@ class LoaderViewModel {
 
   Future<void> checkAuth() async {
     final isAuth = await _authService.isAuth();
-    final nextScreen = isAuth
-        ? MainNavigationRouteNames.mainScreen
-        : MainNavigationRouteNames.auth;
+    final nextScreen = isAuth ? MainNavigationRouteNames.mainScreen : MainNavigationRouteNames.auth;
     Navigator.of(context).pushReplacementNamed(nextScreen);
   }
 }
