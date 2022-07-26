0af9ae092864ae18afa65362fe494155bc5bb8af
srzhik <19srzh88@gmail.com>
2022-07-26 16:39:44 +0400

    try to fix movie details bloc

--- lib/domain/blocs/movie_details_bloc.dart
+++ lib/domain/blocs/movie_details_bloc.dart
@@ -5,6 +5,8 @@ import 'package:comics_db_app/domain/local_entity/movie_details_local.dart';
 import 'package:comics_db_app/domain/services/movie_service.dart';
 import 'package:comics_db_app/ui/widgets/localized_model_storage.dart';
 import 'package:comics_db_app/ui/widgets/movie_details/components/movie_details_data.dart';
+import 'package:comics_db_app/ui/widgets/movie_details/components/poster_data.dart';
+import 'package:comics_db_app/ui/widgets/movie_details/components/trailer_data.dart';
 import 'package:comics_db_app/ui/widgets/movie_details/movie_details_cubit.dart';
 import 'package:flutter/material.dart';
 
@@ -16,63 +18,121 @@ class MovieDetailsEventLoadPage extends MovieDetailsEvent {
   MovieDetailsEventLoadPage(this.locale);
 }
 
-class MovieDetailsContainer {
-  final int movieId;
+class MovieDetailsBlocData {
+  String title;
+  String tagline;
+  bool isLoading;
+  String overview;
 
-  const MovieDetailsContainer({
-    required this.movieId,
+  // MovieDetailsPosterData posterData = MovieDetailsPosterData(title: '', voteCount: 0, popularity: 0);
+  String summary;
+  String releaseDate;
+  String genres;
+
+  // MovieDetailsTrailerData trailerData = MovieDetailsTrailerData();
+  // List<List<MovieDetailsMoviePeopleData>> peopleData = const <List<MovieDetailsMoviePeopleData>>[];
+
+  MovieDetailsBlocData.initial()
+      : title = '',
+        tagline = '',
+        isLoading = true,
+        overview = '',
+  // posterData = const List<MovieDetailsPosterData>[],
+        summary = '',
+        releaseDate = '',
+        genres = '';
+
+  // trailerData = '', '';
+
+  MovieDetailsBlocData({
+    required this.title,
+    required this.tagline,
+    required this.isLoading,
+    required this.overview,
+    // required this.posterData,
+    required this.summary,
+    required this.releaseDate,
+    required this.genres,
+    // required this.trailerData,
+    // required this.peopleData,
+
   });
-
-  MovieDetailsContainer copyWith({
-    int? movieId,
-  }) {
-    return MovieDetailsContainer(
-      movieId: movieId ?? this.movieId,
-    );
-  }
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is MovieDetailsContainer && runtimeType == other.runtimeType && movieId == other.movieId;
+          other is MovieDetailsBlocData && runtimeType == other.runtimeType && title == other.title &&
+              tagline == other.tagline && isLoading == other.isLoading && overview == other.overview &&
+              summary == other.summary && releaseDate == other.releaseDate && genres == other.genres;
 
   @override
-  int get hashCode => movieId.hashCode;
+  int get hashCode =>
+      title.hashCode ^ tagline.hashCode ^ isLoading.hashCode ^ overview.hashCode ^ summary.hashCode ^ releaseDate
+          .hashCode ^ genres.hashCode;
 
-  const MovieDetailsContainer.initial() : movieId = 0;
+  MovieDetailsBlocData copyWith({
+    String? title,
+    String? tagline,
+    bool? isLoading,
+    String? overview,
+    String? summary,
+    String? releaseDate,
+    String? genres,
+  }) {
+    return MovieDetailsBlocData(
+      title: title ?? this.title,
+      tagline: tagline ?? this.tagline,
+      isLoading: isLoading ?? this.isLoading,
+      overview: overview ?? this.overview,
+      summary: summary ?? this.summary,
+      releaseDate: releaseDate ?? this.releaseDate,
+      genres: genres ?? this.genres,
+    );
+  }
+
 }
 
 class MovieDetailsState {
-  final MovieDetailsContainer movieDetailsContainer;
+  final MovieDetailsBlocData movieDetailsBlocData;
 
-  MovieDetailsState({required this.movieDetailsContainer});
+  MovieDetailsState.initial() : movieDetailsBlocData = MovieDetailsBlocData(
+    title: '',
+    tagline: '',
+    isLoading: true,
+    overview: '',
+    // posterData
+    summary: '',
+    releaseDate: '',
+    genres: '',
+    // trailerData: '',
+    // peopleData: '',
+  );
 
-  MovieDetailsContainer get details => movieDetailsContainer;
-
-  MovieDetailsState.initial() : movieDetailsContainer = const MovieDetailsContainer.initial();
+  MovieDetailsState({required this.movieDetailsBlocData});
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is MovieDetailsState &&
-          runtimeType == other.runtimeType &&
-          movieDetailsContainer == other.movieDetailsContainer;
+          other is MovieDetailsState &&
+              runtimeType == other.runtimeType &&
+              movieDetailsBlocData == other.movieDetailsBlocData;
 
   @override
-  int get hashCode => movieDetailsContainer.hashCode;
+  int get hashCode => movieDetailsBlocData.hashCode;
 
   MovieDetailsState copyWith({
-    MovieDetailsContainer? movieDetailsContainer,
+    MovieDetailsBlocData? movieDetailsBlocData,
   }) {
     return MovieDetailsState(
-      movieDetailsContainer: movieDetailsContainer ?? this.movieDetailsContainer,
+      movieDetailsBlocData: movieDetailsBlocData ?? this.movieDetailsBlocData,
     );
   }
+// MovieDetailsBlocData get movieDetails => movieDetailsBlocData;
+
 }
 
 class MovieDetailsBloc extends Bloc<MovieDetailsEvent, MovieDetailsState> {
   // final _movieApiClient = MovieAndTvApiClient();
-
   // final _authService = AuthService();
   final _movieService = MovieService();
   final _localeStorage = LocalizedModelStorage();
@@ -89,13 +149,9 @@ class MovieDetailsBloc extends Bloc<MovieDetailsEvent, MovieDetailsState> {
     );
   }
 
-  Future<MovieDetailsState> movieDetailsLoadPage(
-      MovieDetailsEventLoadPage event, Emitter<MovieDetailsState> emit) async {
+  Future<void> movieDetailsLoadPage(MovieDetailsEventLoadPage event, Emitter<MovieDetailsState> emit) async {
     // final movieId = state.movieDetailsContainer.movieId;
-    try {
-      final details = await _movieService.loadMovieDetails(movieId: movieId, locale: _localeStorage.localeTag);
-    } on ApiClientException catch (e) {}
-    ;
+    final details = await _movieService.loadMovieDetails(movieId: movieId, locale: _localeStorage.localeTag);
     // _handleApiClientException(e, context);
     // if (state.details != null) {
     //   final result = await _movieApiClient.movieDetails(movieId, event.locale);
@@ -106,19 +162,27 @@ class MovieDetailsBloc extends Bloc<MovieDetailsEvent, MovieDetailsState> {
     // emit(newState);
     // updateData(details.details, details.isFavorite, data);
     // return details;
-    final container = state.movieDetailsContainer.copyWith(
-      movieId: movieId,
+    //   final posterData = List<MovieDetailsPosterData>.from(state.movieDetailsBlocData.posterData)..addAll(details.details.)
+    final container = state.movieDetailsBlocData.copyWith(
+      title: details.details.title,
+      tagline: details.details.tagline,
+      overview: details.details.overview,
+      // posterData
+      // trailerData
+      // peopleData
+      // actorsData
+      // summary
+      // releaseDate: details.details.releaseDate,
     );
-    final newState = state.copyWith(movieDetailsContainer: container);
+    final newState = state.copyWith(movieDetailsBlocData: container);
     emit(newState);
-    return newState;
   }
 
   void _handleApiClientException(ApiClientException exception, BuildContext context) {
     switch (exception.type) {
       case ApiClientExceptionType.sessionExpired:
-        // _authService.logout();
-        // MainNavigation.resetNavigation(context);
+      // _authService.logout();
+      // MainNavigation.resetNavigation(context);
         break;
       default:
         print(exception);
