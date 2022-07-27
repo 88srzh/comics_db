2b060ff3dc3fb480152fae4dcbb9ec14117b529f
srzhik <19srzh88@gmail.com>
2022-07-27 16:49:17 +0400

    try to fix bloc

--- lib/ui/widgets/movie_details/components/description_widget.dart
+++ lib/ui/widgets/movie_details/components/description_widget.dart
@@ -15,7 +15,7 @@ class DescriptionWidget extends StatelessWidget {
     //     context.select((MovieDetailsModel model) => model.data.overview);
     var cubit = context.watch<MovieDetailsCubit>();
     var overview = cubit.movieDetailsBloc.state.movieDetailsBlocData.overview;
-    cubit.state
+    // cubit.state
     return Padding(
       padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
       child: Column(
