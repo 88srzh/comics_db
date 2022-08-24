1d10fb2814446e76aaebf0c5529b21c3ac99240a
srzh <19srzh88@gmail.com>
2022-08-24 18:56:21 +0400

    deleta extra code

--- lib/ui/widgets/movie_list/components/now_playing_movie_widget.dart
+++ lib/ui/widgets/movie_list/components/now_playing_movie_widget.dart
@@ -17,27 +17,20 @@ class _NowPlayingMovieWidgetState extends State<NowPlayingMovieWidget> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     final locale = Localizations.localeOf(context);
-    // context.read<NowPlayingMovieListModel>().setupNowPlayingMovieLocale(locale);
     context.read<NowPlayingMovieListCubit>().setupNowPlayingMovieLocale(locale.languageCode);
   }
 
   @override
   Widget build(BuildContext context) {
-    // final nowPlayingMovieModel = context.watch<NowPlayingMovieListModel>();
     var cubit = context.watch<NowPlayingMovieListCubit>();
     return ListView.builder(
       scrollDirection: Axis.horizontal,
-      // itemCount: nowPlayingMovieModel.movies.length,
       itemCount: cubit.state.movies.length,
       itemBuilder: (BuildContext context, int index) {
-        // nowPlayingMovieModel.showedNowPlayingMovieAtIndex(index);
         cubit.showedNowPlayingMovieAtIndex(index);
-        // final nowPlayingMovie = nowPlayingMovieModel.movies[index];
         final movie = cubit.state.movies[index];
-        // final posterPath = nowPlayingMovie.posterPath;
         final posterPath = movie.posterPath;
         return InkWell(
-          // onTap: () => nowPlayingMovieModel.onMovieTap(context, index),
           onTap: () => onMovieTap(context, index),
           child: Padding(
             padding: const EdgeInsets.only(top: 10.0, bottom: 20.0, right: 10.0),
