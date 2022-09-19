0dd550d4bed96e3319cfc84ef8ca14fd8f044d67
srzh <61797666+88srzh@users.noreply.github.com>
2022-09-19 21:40:00 +0400

    step to change people widget

--- lib/ui/widgets/people_widget/popular_people_list_widget.dart
+++ lib/ui/widgets/people_widget/popular_people_list_widget.dart
@@ -1,8 +1,5 @@
 import 'package:comics_db_app/app_colors.dart';
 import 'package:comics_db_app/domain/api_client/image_downloader.dart';
-import 'package:comics_db_app/resources/resources.dart';
-import 'package:comics_db_app/ui/widgets/movie_list/components/movie_list_data.dart';
-import 'package:comics_db_app/ui/widgets/movie_list/movie_list_cubit.dart';
 import 'package:comics_db_app/ui/widgets/people_widget/components/people_list_data.dart';
 import 'package:comics_db_app/ui/widgets/people_widget/popular_people_list_cubit.dart';
 import 'package:flutter/material.dart';
@@ -43,18 +40,23 @@ class _PopularPeopleListWidgetState extends State<PopularPeopleListWidget> {
         color: AppColors.kPrimaryColor,
         child: Stack(
           children: [
-            ListView.builder(
+            GridView.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 100,
+                // childAspectRatio: 4,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+              ),
               keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              padding: const EdgeInsets.only(top: 70.0),
+              // padding: const EdgeInsets.only(top: 70.0),
               itemCount: cubit.state.people.length,
-              itemExtent: 165,
               itemBuilder: (BuildContext context, int index) {
                 cubit.showedPopularPeopleAtIndex(index);
                 final people = cubit.state.people[index];
                 final profilePath = people.profilePath;
                 return InkWell(
                   onTap: () => cubit.onPeopleTap(context, index),
-                  child: _PeoplePopularListRowWidget(profilePath: profilePath, people: people, cubit: cubit, index: index),
+                  child: _PeoplePopularListColumnWidget(profilePath: profilePath, people: people, cubit: cubit, index: index),
                 );
               },
             ),
@@ -91,10 +93,10 @@ class _PopularPeopleListWidgetState extends State<PopularPeopleListWidget> {
   }
 }
 
-class _PeoplePopularListRowWidget extends StatelessWidget {
+class _PeoplePopularListColumnWidget extends StatelessWidget {
   final int index;
 
-  const _PeoplePopularListRowWidget({
+  const _PeoplePopularListColumnWidget({
     Key? key,
     required this.profilePath,
     required this.people,
@@ -108,79 +110,76 @@ class _PeoplePopularListRowWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: AppColors.kPrimaryColor,
-              border: Border.all(color: Colors.white.withOpacity(0.2)),
-              borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.white.withOpacity(0.1),
-                  blurRadius: 8,
-                  offset: const Offset(0, 2),
-                )
-              ],
-            ),
-            clipBehavior: Clip.hardEdge,
-            child: Row(
-              children: [
-                Image.network(
-                  ImageDownloader.imageUrl(profilePath),
-                  width: 95,
-                ),
-                const SizedBox(width: 15.0),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 20.0),
-                      Text(
-                        people.name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 5.0),
-                      // Text(
-                      //   movie.releaseDate,
-                      //   maxLines: 1,
-                      //   overflow: TextOverflow.ellipsis,
-                      //   style: const TextStyle(
-                      //     color: AppColors.genresText,
-                      //     fontSize: 13,
-                      //   ),
-                      // ),
-                      // const SizedBox(height: 20.0),
-                      // Text(
-                      //   movie.overview ?? '',
-                      //   maxLines: 3,
-                      //   overflow: TextOverflow.ellipsis,
-                      //   style: const TextStyle(
-                      //     color: AppColors.genresText,
-                      //     fontSize: 12,
-                      //   ),
-                      // ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 5.0),
-              ],
-            ),
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors.kPrimaryColor,
+            border: Border.all(color: Colors.white.withOpacity(0.2)),
+            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white.withOpacity(0.1),
+                blurRadius: 8,
+                offset: const Offset(0, 2),
+              )
+            ],
           ),
-          // InkWell(
-          //   borderRadius: BorderRadius.circular(20.0),
-          //   onTap: () => _onMovieTap(context, movie.id),
-          // onTap: () => _onMovieTap(context, index),
-          // ),
-        ],
-      ),
+          clipBehavior: Clip.hardEdge,
+          child: Column(
+            children: [
+              Image.network(
+                ImageDownloader.imageUrl(profilePath),
+                width: 95,
+              ),
+              const SizedBox(width: 15.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // const SizedBox(height: .0),
+                    Text(
+                      people.name,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5.0),
+                    // Text(
+                    //   movie.releaseDate,
+                    //   maxLines: 1,
+                    //   overflow: TextOverflow.ellipsis,
+                    //   style: const TextStyle(
+                    //     color: AppColors.genresText,
+                    //     fontSize: 13,
+                    //   ),
+                    // ),
+                    // const SizedBox(height: 20.0),
+                    // Text(
+                    //   movie.overview ?? '',
+                    //   maxLines: 3,
+                    //   overflow: TextOverflow.ellipsis,
+                    //   style: const TextStyle(
+                    //     color: AppColors.genresText,
+                    //     fontSize: 12,
+                    //   ),
+                    // ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 5.0),
+            ],
+          ),
+        ),
+        // InkWell(
+        //   borderRadius: BorderRadius.circular(20.0),
+        //   onTap: () => _onMovieTap(context, movie.id),
+        // onTap: () => _onMovieTap(context, index),
+        // ),
+      ],
     );
   }
 }
