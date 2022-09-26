829e24dda097e2b9ee2e76d1e9e8b38fc5380ec9
srzh <61797666+88srzh@users.noreply.github.com>
2022-09-26 20:08:34 +0400

    add biography

---
+++ lib/ui/widgets/people_details/components/description_widget.dart
@@ -0,0 +1,51 @@
+import 'package:comics_db_app/app_colors.dart';
+import 'package:comics_db_app/ui/widgets/people_details/people_details_cubit.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class DescriptionWidget extends StatelessWidget {
+  const DescriptionWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var cubit = context.watch<PeopleDetailsCubit>();
+    var biography = cubit.state.biography;
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Biography',
+            style: TextStyle(
+              fontSize: 21,
+              fontWeight: FontWeight.w600,
+              // TODO rename text
+              color: AppColors.genresText,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                // TODO: Добавить расстояние между строками
+                child: Text(
+                  biography,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 15,
+                  style: const TextStyle(
+                    color: AppColors.genresText,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/widgets/people_details/people_details_widget.dart
+++ lib/ui/widgets/people_details/people_details_widget.dart
@@ -1,4 +1,5 @@
 import 'package:comics_db_app/app_colors.dart';
+import 'package:comics_db_app/ui/widgets/people_details/components/description_widget.dart';
 import 'package:comics_db_app/ui/widgets/people_details/components/people_top_poster_widget.dart';
 import 'package:comics_db_app/ui/widgets/people_details/people_details_cubit.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +33,7 @@ class _PeopleDetailsWidgetState extends State<PeopleDetailsWidget> {
           Column(
             children: const [
               PeopleTopPosterWidget(),
+              DescriptionWidget(),
               // const DescriptionWidget(),
             ],
           ),
