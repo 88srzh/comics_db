788c8891b737fb3725aeede0d433878f962efb85
srzh <61797666+88srzh@users.noreply.github.com>
2022-10-26 21:02:25 +0400

    add todo

--- lib/ui/widgets/errors/network_connection.dart
+++ lib/ui/widgets/errors/network_connection.dart
@@ -6,42 +6,45 @@ class ConnectionFailedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO may be endure it
     final ButtonStyle style = ElevatedButton.styleFrom(
-      backgroundColor: const Color(0xFF68C581), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),);
+      backgroundColor: const Color(0xFF68C581),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+    );
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: [
-        Image.asset(
-        AppImages.networkConnection,
-        fit: BoxFit.cover,
-      ),
-      Positioned(
-        bottom: MediaQuery.of(context).size.height * 0.15,
-        left: MediaQuery.of(context).size.width * 0.3,
-        right: MediaQuery.of(context).size.width * 0.3,
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                offset: const Offset(0, 13),
-                blurRadius: 25,
-                color: const Color(0xFF5666C2).withOpacity(0.17),
+          Image.asset(
+            AppImages.networkConnection,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height * 0.15,
+            left: MediaQuery.of(context).size.width * 0.3,
+            right: MediaQuery.of(context).size.width * 0.3,
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 13),
+                    blurRadius: 25,
+                    color: const Color(0xFF5666C2).withOpacity(0.17),
+                  ),
+                ],
               ),
-            ],
+              child: ElevatedButton(
+                style: style,
+                onPressed: () {},
+                child: Text(
+                  "retry".toUpperCase(),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
           ),
-          child: ElevatedButton(
-            style: style,
-          onPressed: () {},
-          child: Text(
-            "retry".toUpperCase(),
-            style: const TextStyle(color: Colors.white),
-          ),
-        ),
+        ],
       ),
-    )],
-    )
-    ,
     );
   }
 }
