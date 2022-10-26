73d59dfdfb4ee1e59b48676d028150e0b04b96fa
srzh <61797666+88srzh@users.noreply.github.com>
2022-10-26 21:00:55 +0400

    fix connection failed screen

--- lib/ui/widgets/errors/network_connection.dart
+++ lib/ui/widgets/errors/network_connection.dart
@@ -2,44 +2,46 @@ import 'package:comics_db_app/resources/resources.dart';
 import 'package:flutter/material.dart';
 
 class ConnectionFailedScreen extends StatelessWidget {
+  const ConnectionFailedScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xFF68C581), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),);
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: [
-          Image.asset(
-            AppImages.networkConnection,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.15,
-            left: MediaQuery.of(context).size.width * 0.3,
-            right: MediaQuery.of(context).size.width * 0.3,
-            child: Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 13),
-                    blurRadius: 25,
-                    color: const Color(0xFF5666C2).withOpacity(0.17),
-                  ),
-                ],
-              ),
-              child: FlatButton(
-                color: const Color(0xFF68C581),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50)),
-                onPressed: () {},
-                child: Text(
-                  "retry".toUpperCase(),
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          )
-        ],
+        Image.asset(
+        AppImages.networkConnection,
+        fit: BoxFit.cover,
       ),
+      Positioned(
+        bottom: MediaQuery.of(context).size.height * 0.15,
+        left: MediaQuery.of(context).size.width * 0.3,
+        right: MediaQuery.of(context).size.width * 0.3,
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(0, 13),
+                blurRadius: 25,
+                color: const Color(0xFF5666C2).withOpacity(0.17),
+              ),
+            ],
+          ),
+          child: ElevatedButton(
+            style: style,
+          onPressed: () {},
+          child: Text(
+            "retry".toUpperCase(),
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    )],
+    )
+    ,
     );
   }
 }
