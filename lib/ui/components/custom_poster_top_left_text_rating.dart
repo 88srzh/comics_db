4c0542c155cb380e30344f34cd3513b95657d54b
srzh <61797666+88srzh@users.noreply.github.com>
2022-11-25 23:28:52 +0400

    refactoring custom text widget

--- lib/ui/components/custom_poster_top_left_text_rating.dart
+++ lib/ui/components/custom_poster_top_left_text_rating.dart
@@ -1,9 +1,9 @@
 import 'package:comics_db_app/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class CustomPosterTopLeftTextRating extends StatelessWidget {
+class CustomPosterTopLeftAlignTextRating extends StatelessWidget {
   final String text;
-  const CustomPosterTopLeftTextRating({Key? key, required this.text}) : super(key: key);
+  const CustomPosterTopLeftAlignTextRating({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
