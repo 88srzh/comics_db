ba6235b3ef3803969ac7a4ad4a91ff02fb22cc14
srzhik <19srzh88@gmail.com>
2022-12-08 15:26:47 +0400

    added tv popular list cubit

--- lib/ui/widgets/tv_list/tv_list_cubit_state.dart
+++ lib/ui/widgets/tv_list/tv_list_cubit_state.dart
@@ -5,4 +5,22 @@ class TvListCubitState {
   final String localeTag;
 
   TvListCubitState({required this.tvs, required this.localeTag});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TvListCubitState && runtimeType == other.runtimeType && tvs == other.tvs && localeTag == other.localeTag;
+
+  @override
+  int get hashCode => tvs.hashCode ^ localeTag.hashCode;
+
+  TvListCubitState copyWith({
+    List<TvListData>? tvs,
+    String? localeTag,
+  }) {
+    return TvListCubitState(
+      tvs: tvs ?? this.tvs,
+      localeTag: localeTag ?? this.localeTag,
+    );
+  }
 }
