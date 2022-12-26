7a9ca61245c340a10e91420a14e43af06872866e
srzhik <19srzh88@gmail.com>
2022-12-26 14:46:18 +0400

    added custom search input decoration

--- lib/ui/components/custom_search_bar_widget.dart
+++ lib/ui/components/custom_search_bar_widget.dart
@@ -1,9 +1,11 @@
 import 'package:comics_db_app/app_colors.dart';
+import 'package:comics_db_app/ui/components/custom_search_input_decoration_widget.dart';
 import 'package:flutter/material.dart';
 
 class CustomSearchBar extends StatelessWidget {
   final Function(String)? onChanged;
-  const CustomSearchBar({Key? key,required this.onChanged}) : super(key: key);
+
+  const CustomSearchBar({Key? key, required this.onChanged}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,23 +14,7 @@ class CustomSearchBar extends StatelessWidget {
         color: AppColors.genresText,
       ),
       onChanged: onChanged,
-      decoration: InputDecoration(
-        labelText: 'Search',
-        labelStyle: const TextStyle(
-          color: AppColors.genresText,
-        ),
-        filled: true,
-        fillColor: AppColors.kPrimaryColor,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white.withOpacity(0.2)),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white.withOpacity(0.2)),
-        ),
-      ),
+      decoration: customSearchInputDecoration(text: 'Search'),
     );
   }
 }
