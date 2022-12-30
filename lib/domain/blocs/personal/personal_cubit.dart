1827eca8261a412e8a7d4ebb71ee34fe9bcb22dc
srzh <61797666+88srzh@users.noreply.github.com>
2022-12-30 19:47:22 +0400

    added personal state

--- lib/domain/blocs/personal/personal_cubit.dart
+++ lib/domain/blocs/personal/personal_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:comics_db_app/domain/blocs/personal/personal_container.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'personal_state.dart';
--- lib/domain/blocs/personal/personal_state.dart
+++ lib/domain/blocs/personal/personal_state.dart
@@ -1,6 +1,20 @@
 part of 'personal_cubit.dart';
 
-@immutable
-abstract class PersonalState {}
+class PersonalDetailsState extends Equatable {
+  final AccountDetailsContainer accountDetailsContainer;
 
-class PersonalInitial extends PersonalState {}
+  const PersonalDetailsState.initial() : accountDetailsContainer = const AccountDetailsContainer.initial();
+
+  const PersonalDetailsState({required this.accountDetailsContainer});
+
+  @override
+  List<Object> get props => [accountDetailsContainer];
+
+  PersonalDetailsState copyWith({
+    AccountDetailsContainer? accountDetailsContainer,
+  }) {
+    return PersonalDetailsState(
+        accountDetailsContainer: accountDetailsContainer ?? this.accountDetailsContainer,
+    );
+  }
+}
