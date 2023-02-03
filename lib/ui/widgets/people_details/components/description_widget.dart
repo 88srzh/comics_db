c59fcc5493360fa2817ddb8203816852f18045da
srzhik <19srzh88@gmail.com>
2023-02-03 15:41:04 +0400

    changed header color to light theme

--- lib/ui/widgets/people_details/components/description_widget.dart
+++ lib/ui/widgets/people_details/components/description_widget.dart
@@ -1,4 +1,5 @@
 // Flutter imports:
+import 'package:comics_db_app/ui/widgets/settings/model_theme.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
@@ -19,32 +20,36 @@ class DescriptionWidget extends StatelessWidget {
     var cubit = context.watch<PeopleDetailsCubit>();
     var biography = cubit.state.biography;
     if (biography.isEmpty) return const SizedBox.shrink();
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Biography',
-            style: TextStyle(
-              fontSize: 21,
-              fontWeight: FontWeight.w600,
-              // TODO rename text
-              color: AppColors.genresText,
-            ),
-          ),
-          // if i have empty biography i will have extra sizedbox
-          const SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+    return Consumer<ModelTheme>(
+      builder: (context, ModelTheme notifierTheme, child) {
+        return Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
-                child: CustomDescriptionExpandableText(description: biography),
+               Text(
+                'Biography',
+                style: TextStyle(
+                  fontSize: 21,
+                  fontWeight: FontWeight.w600,
+                  // TODO rename text
+                  color: notifierTheme.isDark ? AppColors.genresText : AppColors.headerText,
+                ),
+              ),
+              // if i have empty biography i will have extra sizedbox
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: CustomDescriptionExpandableText(description: biography),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
