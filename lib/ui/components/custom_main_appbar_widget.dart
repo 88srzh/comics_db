c1263dbe81e0815ce0071539ee7ecd1cd93e97a7
srzhik <19srzh88@gmail.com>
2023-03-03 16:00:53 +0400

    step to fix appbar to switch themes

--- lib/ui/components/custom_main_appbar_widget.dart
+++ lib/ui/components/custom_main_appbar_widget.dart
@@ -1,9 +1,9 @@
 // Flutter imports:
+import 'package:comics_db_app/domain/blocs/theme/theme_bloc.dart';
 import 'package:flutter/material.dart';
 
 // Project imports:
-import 'package:comics_db_app/app_colors.dart';
-import 'package:comics_db_app/resources/resources.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomMainAppBarWidget extends StatelessWidget with PreferredSizeWidget {
   const CustomMainAppBarWidget({Key? key}) : super(key: key);
@@ -12,53 +12,10 @@ class CustomMainAppBarWidget extends StatelessWidget with PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       titleSpacing: 0.0,
-      title: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Image.asset(AppImages.movieAppBarLogo),
-              ],
-            ),
-            // Row(
-            //   crossAxisAlignment: CrossAxisAlignment.center,
-            //   mainAxisAlignment: MainAxisAlignment.end,
-            //   children: [
-            //     GestureDetector(
-            //       onTap: () {},
-            //       child: const Icon(
-            //         Icons.search,
-            //         color: AppColors.searchIcon,
-            //         size: 30,
-            //       ),
-            //     ),
-            //     Padding(
-            //       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            //       child: GestureDetector(
-            //         onTap: () {},
-            //         child: const Icon(
-            //           Icons.filter_alt_outlined,
-            //           color: AppColors.ratingThumb,
-            //           size: 30,
-            //         ),
-            //       ),
-            //     ),
-            //     GestureDetector(
-            //       onTap: () {},
-            //       child: const Icon(
-            //         Icons.menu,
-            //         color: AppColors.ratingThumb,
-            //         size: 30,
-            //       ),
-            //     ),
-            //   ],
-            // ),
-          ],
-        ),
+      leading: IconButton(
+        icon: const Icon(Icons.icecream_outlined),
+        onPressed: () => context.read<ThemeBloc>().add(const ThemeEvent()),
       ),
-      backgroundColor: AppColors.kPrimaryColor,
     );
   }
 
--- lib/ui/widgets/movie_list/movie_list_widget.dart
+++ lib/ui/widgets/movie_list/movie_list_widget.dart
@@ -1,4 +1,5 @@
 // Flutter imports:
+import 'package:comics_db_app/domain/blocs/theme/theme_bloc.dart';
 import 'package:comics_db_app/ui/components/custom_header_text_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,7 @@ import 'package:comics_db_app/ui/widgets/movie_list/components/now_playing_movie
 import 'package:comics_db_app/ui/widgets/movie_list/components/popular_movie_widget.dart';
 import 'package:comics_db_app/ui/widgets/movie_list/components/top_rated_movie_widget.dart';
 import 'package:comics_db_app/ui/widgets/movie_list/components/upcoming_movie_widget.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Package imports:
 
@@ -23,70 +25,35 @@ class MovieListWidget extends StatelessWidget {
     // TODO: after refactoring search doesn't work
     // content: _SearchWidget(),
     // );
-        return Scaffold(
-          appBar: const CustomMainAppBarWidget(),
-          body: ColoredBox(
-            color: AppColors.kPrimaryColor,
-            child: ListView(
+    return Scaffold(
+      appBar: const CustomMainAppBarWidget(),
+      // appBar: AppBar(
+      //     leading: IconButton(
+      //       icon: const Icon(Icons.icecream),
+      //       onPressed: () => context.read<ThemeBloc>().add(const ThemeEvent()),
+      //     ),
+      // ),
+      body: ColoredBox(
+        color: AppColors.kPrimaryColor,
+        child: ListView(
+          children: [
+            Column(
               children: [
-                Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(top: 20.0, left: 20.0, bottom: 20.0),
-                      child: SizedBox(
-                        height: 180,
-                        child: TopRatedMovieWidget(),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const CustomHeaderTextWidget(text: 'Popular'),
-                          InkWell(
-                            onTap: () => Navigator.of(context).pushNamed(MainNavigationRouteNames.popularMovie),
-                            child: const Text(
-                              'See All',
-                              style: TextStyle(
-                                color: AppColors.ratingText,
-                                fontSize: 15,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 200,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20.0),
-                        child: PopularMovieWidget(),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          CustomHeaderTextWidget(text: 'Coming soon'),
-                        ],
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.0),
-                      child: UpcomingMovieWidget(),
-                    ),
-                  ],
+                const Padding(
+                  padding: EdgeInsets.only(top: 20.0, left: 20.0, bottom: 20.0),
+                  child: SizedBox(
+                    height: 180,
+                    child: TopRatedMovieWidget(),
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const CustomHeaderTextWidget(text: 'Now playing'),
+                      const CustomHeaderTextWidget(text: 'Popular'),
                       InkWell(
-                        onTap: () => Navigator.of(context).pushNamed(MainNavigationRouteNames.nowPlayingMovie),
+                        onTap: () => Navigator.of(context).pushNamed(MainNavigationRouteNames.popularMovie),
                         child: const Text(
                           'See All',
                           style: TextStyle(
@@ -102,12 +69,53 @@ class MovieListWidget extends StatelessWidget {
                   height: 200,
                   child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: NowPlayingMovieWidget(),
+                    child: PopularMovieWidget(),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      CustomHeaderTextWidget(text: 'Coming soon'),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: UpcomingMovieWidget(),
                 ),
               ],
             ),
-          ),
-        );
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const CustomHeaderTextWidget(text: 'Now playing'),
+                  InkWell(
+                    onTap: () => Navigator.of(context).pushNamed(MainNavigationRouteNames.nowPlayingMovie),
+                    child: const Text(
+                      'See All',
+                      style: TextStyle(
+                        color: AppColors.ratingText,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 200,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: NowPlayingMovieWidget(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
