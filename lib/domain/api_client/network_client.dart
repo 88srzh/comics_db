187dafd108867f37347fb0d83aeca3da4ca326c0
srzhik <19srzh88@gmail.com>
2023-05-11 15:56:14 +0400

    expanded errors

--- lib/domain/api_client/network_client.dart
+++ lib/domain/api_client/network_client.dart
@@ -82,13 +82,55 @@ class NetworkClient {
     if (response.statusCode == 401) {
       final dynamic status = json['status_code'];
       final code = status is int ? status : 0;
-      if (code == 30) {
-        throw ApiClientException(ApiClientExceptionType.auth);
-      } else if (code == 3) {
-        throw ApiClientException(ApiClientExceptionType.sessionExpired);
-      } else {
-        throw ApiClientException(ApiClientExceptionType.other);
+      // TODO need refactoring
+      switch (code) {
+        case 30:
+          if (kDebugMode) {
+            print('Invalid username and/or password: You did not provide a valid login.');
+          }
+          throw ApiClientException(ApiClientExceptionType.auth);
+        case 3:
+          if (kDebugMode) {
+            print('Authentication failed: You do not have permissions to access the service.');
+          }
+          throw ApiClientException(ApiClientExceptionType.sessionExpired);
+        case 7:
+          if (kDebugMode) {
+            print('Invalid API key: You must be granted a valid key.');
+          }
+          break;
+        case 10:
+          if (kDebugMode) {
+            print('Suspended APU key: Access to  your account has been suspended, contact TMDB.');
+          }
+          break;
+        case 14:
+          if (kDebugMode) {
+            print('Authentication failed.');
+          }
+          break;
+        case 32:
+          if (kDebugMode) {
+            print('Email not verified: Your email address has not been verified.');
+          }
+          break;
+        case 33:
+          if (kDebugMode) {
+            print('Invalid request token: The request token is either expired or invalid.');
+          }
+          break;
+        case 35:
+          if (kDebugMode) {
+            print('Invalid token.');
+          }
+          break;
+        case 36:
+          if (kDebugMode) {
+            print('This token hasnt been granted write permission by the user.');
+                break;
+          }
       }
+      throw ApiClientException(ApiClientExceptionType.other);
     }
   }
 }
