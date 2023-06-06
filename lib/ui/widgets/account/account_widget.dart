8645554d103c0e542709b1981fcf8d6dcd00a2f5
srzh <61797666+88srzh@users.noreply.github.com>
2023-06-06 17:36:32 +0400

    added custom account list tile for favorites

--- lib/ui/widgets/account/account_widget.dart
+++ lib/ui/widgets/account/account_widget.dart
@@ -5,7 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 // Project imports:
+import 'package:comics_db_app/ui/components/custom_account_list_tile.dart';
 import 'package:comics_db_app/ui/components/custom_setting_divider_widget.dart';
+import 'package:comics_db_app/ui/navigation/main_navigation.dart';
 import 'package:comics_db_app/ui/widgets/account/account_details_cubit.dart';
 import 'package:comics_db_app/ui/widgets/account/components/heading_account_card_widget.dart';
 import 'package:comics_db_app/ui/widgets/account/components/logout_card_widget.dart';
@@ -50,9 +52,12 @@ class _BodyPersonalWidgetState extends State<BodyPersonalWidget> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         HeadingAccountCardWidget(headingText: name),
-        const CustomDropdownMenuItemWidget(),
         const CustomSettingDivider(),
-        const LogoutCardWidget(),
+        const HeadingAccountCardWidget(headingText: 'Favorites'),
+        const CustomSettingDivider(),
+        CustomAccountListTile(text: 'Movies', onTap: () => Navigator.of(context).pushNamed(MainNavigationRouteNames.favoriteMovies)),
+        const CustomSettingDivider(),
+        CustomAccountListTile(text: 'TV', onTap: () => Navigator.of(context).pushNamed(MainNavigationRouteNames.favoriteTvs)),
         const CustomSettingDivider(),
         const HeadingAccountCardWidget(headingText: 'Settings'),
         const CustomSettingDivider(),
@@ -60,51 +65,11 @@ class _BodyPersonalWidgetState extends State<BodyPersonalWidget> {
         const CustomSettingDivider(),
         const NotificationsCardWidget(),
         const CustomSettingDivider(),
+        const LogoutCardWidget(),
+        const CustomSettingDivider(),
         // const AnimationFab(),
       ],
     );
-  }
-}
-
-class CustomDropdownMenuItemWidget extends StatefulWidget {
-  const CustomDropdownMenuItemWidget({
-    super.key,
-  });
-
-  @override
-  State<CustomDropdownMenuItemWidget> createState() => _CustomDropdownMenuItemWidgetState();
-}
-
-class _CustomDropdownMenuItemWidgetState extends State<CustomDropdownMenuItemWidget> {
-  @override
-  Widget build(BuildContext context) {
-    // String dropDownValue = 'Favorites';
-
-    return DropdownButton<String>(
-      // items: const ['Favorites', 'Movies', 'TV'].map((String value) {
-      //   return DropdownMenuItem<String>(
-      //     value: value,
-      //     child: Text(value),
-      //   );
-      // }).toList(),
-      items: const [
-        DropdownMenuItem(value: 'Favorites', child: Text('Favorites')),
-        DropdownMenuItem(value: 'Movies', child: Text('Movies')),
-        DropdownMenuItem(value: 'TV', child: Text('TV')),
-      ],
-      value: dropDownValue,
-      isExpanded: true,
-      style: const TextStyle(color: Colors.green),
-      onChanged: dropDownCallback,
-    );
-  }
-
-  void dropDownCallback(String? selectedValue) {
-    if (selectedValue is String) {
-      setState(() {
-        dropDownValue = selectedValue;
-      });
-    }
   }
 }
 
--- lib/ui/widgets/favorite_screen/favorite_screen_widget.dart
+++ lib/ui/widgets/favorite_screen/favorite_screen_widget.dart
@@ -1,13 +1,15 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:animated_button_bar/animated_button_bar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+// Project imports:
 import 'package:comics_db_app/core/dark_theme_colors.dart';
 import 'package:comics_db_app/ui/components/custom_appbar_widget.dart';
 import 'package:comics_db_app/ui/widgets/favorite_screen/movie/favorite_movie_list_cubit.dart';
 import 'package:comics_db_app/ui/widgets/favorite_screen/movie/favorite_movie_list_widget.dart';
-import 'package:comics_db_app/ui/widgets/favorite_screen/tv/favorite_tv_list_widget.dart';
-import 'package:flutter/material.dart';
-
-// Project imports:
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FavoriteScreenWidget extends StatefulWidget {
   const FavoriteScreenWidget({Key? key}) : super(key: key);
