5f1f9c477de2fb8f5c1cca992f0b0d05cfd37f4b
srzhik <19srzh88@gmail.com>
2023-06-23 15:00:52 +0400

    caught error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // @dart=2.9
 
 // Flutter imports:
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 //
 
@@ -8,6 +11,10 @@ import 'package:flutter/material.dart';
 import 'package:comics_db_app/ui/widgets/app/my_app.dart';
 
 void main() {
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    if (kReleaseMode) exit(1);
+  };
   const app = MyApp();
   runApp(app);
 }
