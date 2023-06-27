55ed22ac664d62e7303b7404ebd2c53a7138bbfa
srzhik <19srzh88@gmail.com>
2023-06-27 10:08:19 +0400

    remove extra code

--- lib/domain/services/movie_service.dart
+++ lib/domain/services/movie_service.dart
@@ -1,31 +1,13 @@
 // Project imports:
-import 'package:comics_db_app/configuration/configuration.dart';
 import 'package:comics_db_app/domain/api_client/account_api_client.dart';
 import 'package:comics_db_app/domain/api_client/movie_and_tv_api_client.dart';
 import 'package:comics_db_app/domain/data_providers/session_data_provider.dart';
-import 'package:comics_db_app/domain/entity/movie_response.dart';
 import 'package:comics_db_app/domain/local_entity/movie_details_local.dart';
 
 class MovieService {
   final _movieAndTvApiClient = MovieAndTvApiClient();
   final _sessionDataProvider = SessionDataProvider();
   final _accountApiClient = AccountApiClient();
-
-  // TODO no usage, may be delete
-  Future<MovieResponse> popularMovie(int page, String locale) async =>
-      _movieAndTvApiClient.popularMovie(page, locale, Configuration.apiKey);
-
-  Future<MovieResponse> topRatedMovie(int page, String locale) async =>
-      _movieAndTvApiClient.topRatedMovie(page, locale, Configuration.apiKey);
-
-  Future<MovieResponse> nowPlayingMovie(int page, String locale) async =>
-      _movieAndTvApiClient.nowPlayingMovie(page, locale, Configuration.apiKey);
-
-  Future<MovieResponse> upcomingMovie(int page, String locale) async =>
-      _movieAndTvApiClient.upcomingMovie(page, locale, Configuration.apiKey);
-
-  Future<MovieResponse> searchMovie(int page, String locale, String query) async =>
-      _movieAndTvApiClient.searchMovie(page, locale, query, Configuration.apiKey);
 
   Future<MovieDetailsLocal> loadMovieDetails({required int movieId, required String locale}) async {
     final movieDetails = await _movieAndTvApiClient.movieDetails(movieId, locale);
