93a5a013f0b256af126077daf2fc3458f51ed026
srzh <61797666+88srzh@users.noreply.github.com>
2023-08-20 11:56:36 +0400

    fix temporary certificaties

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
-// Flutter imports:
+// Dart imports:
 import 'dart:io';
 
+// Flutter imports:
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-//
 
 // Project imports:
+import 'package:comics_db_app/ui/components/hhtp_overrides.dart';
 import 'package:comics_db_app/ui/widgets/app/my_app.dart';
+
+//
+
 
 void main() {
   FlutterError.onError = (details) {
@@ -14,5 +18,6 @@ void main() {
     if (kReleaseMode) exit(1);
   };
   const app = MyApp();
+  HttpOverrides.global = MyHttpOverrides();
   runApp(app);
 }
