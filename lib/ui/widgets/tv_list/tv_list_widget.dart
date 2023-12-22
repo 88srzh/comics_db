b4be8fa05a688f58bcc6ee6e84ec04e0838544d8
hu <19srzh88@gmail.com>
2023-12-22 14:28:47 +0400

    deleted discover popular tv bloc and top rated tv model

--- lib/ui/widgets/tv_list/tv_list_widget.dart
+++ lib/ui/widgets/tv_list/tv_list_widget.dart
@@ -1,5 +1,4 @@
 // Flutter imports:
-import 'package:comics_db_app/ui/widgets/tv_discover_list/tv_discover_list_widget.dart';
 import 'package:flutter/material.dart';
 
 // Project imports:
@@ -48,21 +47,6 @@ class TvListWidget extends StatelessWidget {
                 child: Padding(
                   padding: EdgeInsets.symmetric(horizontal: 20.0),
                   child: PopularTvWidget(),
-                ),
-              ),
-              const Padding(padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomHeaderTextWidget(text: 'Discover TV'),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 200,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: DiscoverTvsWidget(),
                 ),
               ),
               const Padding(
