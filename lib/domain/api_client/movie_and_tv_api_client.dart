2955a029c6a24b947a1a136e22823e95bb48ca8f
hu <19srzh88@gmail.com>
2023-12-26 14:36:11 +0400

    fixed discover popular movie method

--- lib/domain/api_client/movie_and_tv_api_client.dart
+++ lib/domain/api_client/movie_and_tv_api_client.dart
@@ -92,7 +92,7 @@ class MovieAndTvApiClient {
     }
 
     final result = _networkClient.get(
-      '/movie/popular',
+      '/discover/movie',
       parser,
       <String, dynamic>{
         'api_key': apiKey,
