0d6a3d740267d5608430aa80c03cf170e0d994cc
hu <19srzh88@gmail.com>
2024-01-12 16:24:00 +0400

    remove extra code

--- lib/domain/api_client/movie_and_tv_api_client.dart
+++ lib/domain/api_client/movie_and_tv_api_client.dart
@@ -175,41 +175,6 @@ class MovieAndTvApiClient {
     return result;
   }
 
-  Future<MovieResponse> discoverUpcomingMovie(
-    int page,
-    String locale,
-    String apiKey,
-    bool includeAdult,
-    bool includeVideo,
-    String sortBy,
-    int withReleaseType,
-    String releaseDateGte,
-    String releaseDateLte,
-  ) async {
-    MovieResponse parser(dynamic json) {
-      final jsonMap = json as Map<String, dynamic>;
-      final response = MovieResponse.fromJson(jsonMap);
-      return response;
-    }
-
-    final result = _networkClient.get(
-      '/discover/movie',
-      parser,
-      <String, dynamic>{
-        'api_key': apiKey,
-        'page': page.toString(),
-        'language': locale,
-        'include_adult': includeAdult.toString(),
-        'include_video': includeVideo.toString(),
-        'sort_by': sortBy,
-        'with_release_type': withReleaseType.toString(),
-        'release_date.gte': releaseDateGte,
-        'release_date.lte': releaseDateLte,
-      },
-    );
-    return result;
-  }
-
   Future<MovieResponse> upcomingMovie(
     int page,
     String locale,
