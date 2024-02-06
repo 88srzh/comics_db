e03bfdc097f2d73f6f490e0102301457de8c71e7
hu <19srzh88@gmail.com>
2024-02-06 16:27:17 +0400

    step to fix guest account widget

--- lib/ui/widgets/account/accounts_widget.dart
+++ lib/ui/widgets/account/accounts_widget.dart
@@ -1,9 +1,7 @@
-import 'package:comics_db_app/domain/api_client/auth_api_client.dart';
-import 'package:comics_db_app/ui/widgets/account/account_details_cubit.dart';
+import 'package:comics_db_app/domain/data_providers/session_data_provider.dart';
 import 'package:comics_db_app/ui/widgets/account/account_widget.dart';
 import 'package:comics_db_app/ui/widgets/account/guest_account_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AccountsWidget extends StatefulWidget {
   const AccountsWidget({super.key});
@@ -14,21 +12,30 @@ class AccountsWidget extends StatefulWidget {
 
 class _AccountsWidgetState extends State<AccountsWidget> {
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    // TODO if guest session - what to do?
-    context.read<AccountDetailsCubit>().setupAccountDetails(context);
+  void initState() {
+    super.initState();
   }
-
-  final _authApiClient = AuthApiClient();
 
   @override
   Widget build(BuildContext context) {
-    final cubit = context.watch<AccountDetailsCubit>();
-    final username = cubit.state.username;
-    // final guestSessionId = authApiClient.guestAuth();
-    // return guestSessionId != null ? const GuestAccountWidget() : const AccountWidget();
-    return cubit != null ? const AccountWidget() : const GuestAccountWidget();
-    // return const AccountWidget();
+    int id;
+    // final id = session.getAccountId();
+
+    // if (id != null) {
+    //   return const AccountWidget();
+    // } else {
+    //   return const GuestAccountWidget();
+    // }
+    Future<Widget?> returnWidget() async {
+      SessionDataProvider session = SessionDataProvider();
+      final id = session.getAccountId();
+      if (id != null) {
+        const AccountWidget();
+      } else {
+        const GuestAccountWidget();
+      }
+      return null;
+    }
+    return returnWidget;
   }
 }
