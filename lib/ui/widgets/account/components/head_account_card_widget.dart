dac9d89d312c3b11d0cf0cccfa506fdb38f0114d
srzh <61797666+88srzh@users.noreply.github.com>
2024-03-23 19:56:08 +0400

    disable account avatar via not fix

--- lib/ui/widgets/account/components/head_account_card_widget.dart
+++ lib/ui/widgets/account/components/head_account_card_widget.dart
@@ -2,13 +2,9 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Project imports:
-import 'package:comics_db_app/domain/api_client/image_downloader.dart';
-import 'package:comics_db_app/resources/resources.dart';
-import 'package:comics_db_app/ui/components/loading_indicator_widget.dart';
 import 'package:comics_db_app/ui/widgets/account/account_details_cubit.dart';
 
 class HeadAccountCardWidget extends StatelessWidget {
@@ -18,12 +14,12 @@ class HeadAccountCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var cubit = context.watch<AccountDetailsCubit>();
     final String username = cubit.state.username;
-    final String? avatarPath = cubit.state.avatarPath;
+    // final String? avatarPath = cubit.state.avatarPath;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
         children: [
-          avatarPath != null
+        /*  avatarPath != null
               ? CircleAvatar(
                   radius: 40.0,
                   backgroundColor: Colors.transparent,
@@ -36,7 +32,7 @@ class HeadAccountCardWidget extends StatelessWidget {
                     ),
                   ),
                 )
-              : Image.asset(AppImages.noImageAvailable),
+              : Image.asset(AppImages.noImageAvailable),*/
           const SizedBox(width: 15.0),
           Text(
             username,
