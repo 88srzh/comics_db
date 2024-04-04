42f19ce54c0b0cc7024976bae4617091ec63b202
hu <19srzh88@gmail.com>
2024-04-04 10:20:50 +0400

    fixed people details length of actors data

--- lib/ui/widgets/movie_details/components/movie_details_cast_and_crew_widget.dart
+++ lib/ui/widgets/movie_details/components/movie_details_cast_and_crew_widget.dart
@@ -66,7 +66,7 @@ class _MovieActorListWidget extends StatelessWidget {
     var actorsData = context.watch<MovieDetailsCubit>().data.actorsData;
     if (actorsData.isEmpty) return const SizedBox.shrink();
     return ListView.builder(
-      itemCount: 8,
+      itemCount: actorsData.length,
       itemExtent: 120,
       scrollDirection: Axis.horizontal,
       itemBuilder: (BuildContext context, int index) {
